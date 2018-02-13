956a715d536fa2e7915677e8d46bc27bf7772ed7
oleh.lesiuk <oleh.lesiuk@falest.pl>
2018-02-13 07:47:26 +0100

    added configuration of health check endpoint; added endpoint in order to verify configuration is updated

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,31 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableWebMvc
+@RestController
+@RefreshScope
 public class DemoApplication {
+
+	@Value("${some.value}")
+	private String value;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@RequestMapping("/value")
+	public String endpoint() {
+		return value;
+	}
+
 }
